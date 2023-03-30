c765e261d52b28d50974279860a0a08889a9a885
Devansh Srivastava <devanshsks@gmail.com>
2023-03-30 19:06:47 +0530

    basic routes in app

--- clientapp/video/src/App.js
+++ clientapp/video/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Login from './pages/login/login';
+import Register from './pages/register/register';
+import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { AuthContext } from './pages/authContext/AuthContext';
 import './App.css';
+import { useContext } from 'react';
 
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element = {user ? <Home /> : <Login />} />
+      </Routes>
+      </Router>
   );
 }
 

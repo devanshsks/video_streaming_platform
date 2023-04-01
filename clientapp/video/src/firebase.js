968f53bdbc2b555e76bc36fd67b998ff5f0c6ce4
Devansh Srivastava <devanshsks@gmail.com>
2023-04-01 23:29:24 +0530

    firebase installed

---
+++ clientapp/video/src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage} from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAMrELSZsJmTXjjwk_pumgt1FXGZ0CwoTw",
+  authDomain: "bestappintheworld-67a98.firebaseapp.com",
+  projectId: "bestappintheworld-67a98",
+  storageBucket: "bestappintheworld-67a98.appspot.com",
+  messagingSenderId: "36541471714",
+  appId: "1:36541471714:web:e3d8ed084746c19caa37bb",
+  measurementId: "G-VXJY7PWTEJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const storage = getStorage(app);

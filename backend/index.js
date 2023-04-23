31cc5034e8f16191cb2785dbd3390912c3445286
Devansh Srivastava <devanshsks@gmail.com>
2023-04-23 21:28:21 +0530

    favorite feature added

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import authroute from "./routes/Auth.js"
 import uploadroute from "./routes/Upload.js"
 import mediaroute from "./routes/Media.js"
+import userroute from "./routes/User.js"
 const app = express();
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use("/api/auth", authroute);
 app.use("/api/upload",uploadroute);
 app.use("/api/media",mediaroute);
+app.use("/api/user",userroute);
 
 app.listen(process.env.PORT, () => {
     console.log("Backend server is running");
--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,1 +1,60 @@
 import express from "express";
+const router = express.Router();
+import Media from "../models/media.js";
+import verify from "../verifyToken.js";
+import mongoose from "mongoose";
+import User from "../models/user.js";
+
+router.post("/addtoFavorites", verify, async (req, res) => {
+    const { mediaId } = req.body;
+    const userId = req.user.id;
+    try {
+        await User.findByIdAndUpdate(
+            userId,
+            {
+              $addToSet: {
+                favorites: mediaId,
+              },
+            },
+            { new: true });
+            res.json("Successfully added to favorites");
+    } catch (err) {
+        res.status(422).json({error: err});
+    }
+  });
+  
+  router.post("/removefromFavorites", verify, async (req, res) => {
+    const { mediaId } = req.body;
+    const userId = req.user.id;
+    try {
+        await User.findByIdAndUpdate(
+            userId,
+            {
+              $pull: {
+                favorites: mediaId,
+              },
+            },
+            { new: true });
+            res.json("Successfully removed from favorites");
+    } catch (err) {
+        res.status(422).json({error: err});
+    }
+  });
+  
+  router.post("/isFavorite", verify, async (req, res) => {
+    const { mediaId } = req.body;
+    const userId = req.user.id;
+    try {
+        const result = await User.findById(userId);
+        if (result.favorites.includes(mediaId)) {
+            res.json(true);
+          } else {
+            res.json(false);
+          }
+    } catch (error) {
+        res.status(422).json({ error: error });
+    }
+
+  });
+
+  export default router;

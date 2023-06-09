3cd76b0b2ee89ae53b5e7dde0889d52f79801c40
Devansh Srivastava <devanshsks@gmail.com>
2023-06-09 16:42:24 +0530

    Room schema made

---
+++ backend/models/room.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const RoomSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    admin: {
+      type: String,
+      required: true,
+    },
+    key: { type: String, required: true },
+    media: { type: String, default: "" },
+    userArray: [
+      {
+        userId: { type: String, required: true },
+        username: { type: String, required: true },
+      },
+    ],
+    bannedUsers: [
+      {
+        userId: { type: String, required: true },
+        username: { type: String, required: true },
+      },
+    ],
+    messages: [
+      {
+        userId: { type: String, required: true },
+        username: { type: String, required: true },
+        message: { type: String, required: true },
+      },
+    ],
+    isPrivate: { type: Boolean, default: false },
+  })
+
+  const Room = mongoose.model("Room", RoomSchema);
+
+  export default Room;
